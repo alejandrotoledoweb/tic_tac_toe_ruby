a7a309ea325baea840ade9f6a92427540b422ce3
410AngelaVu <andjela1993@gmail.com>
2020-10-21 20:01:19 +0200

    added players info class and choose-move class

--- bin/main.rb
+++ bin/main.rb
@@ -20,7 +20,7 @@ class BeginRandom
   def coin_flip
     coin = rand(2)
 
-    if coin = 1
+    if coin == 1
       puts 'we get HEADS UP'
       return true
     else 
@@ -51,4 +51,79 @@ class Board
     puts ""
   end  
 end
+
+def choose_move
+  case $chosen_move
+  when 1
+    if @cells[2][0].empty?
+      @cells[2][0] = "#{$current_player.symbol}" 
+    else 
+      return "Choose another cell"
+    end
+  when 2 
+    if @cells[2][1].empty?
+      @cells[2][1] = "#{$current_player.symbol}" 
+    else
+      return "Choose another cell"
+    end
+  when 3
+    if @cells[2][2].empty?
+      @cells[2][2] = "#{$current_player.symbol}" 
+      else 
+      return "Choose another cell"
+    end
+  when 4
+    if @cells[1][0].empty?
+      @cells[1][0] = "#{$current_player.symbol}" 
+    else 
+      return "Choose another cell"
+    end
+  when 5
+    if @cells[1][1].empty?
+      @cells[1][1] = "#{$current_player.symbol}" 
+    else 
+      return "Choose another cell"
+    end
+when 6
+if @cells[1][2].empty?
+  @cells[1][2] = "#{$current_player.symbol}" 
+else 
+  return "Choose another cell"
+end
+when 7
+if @cells[0][0].empty?
+  @cells[0][0] = "#{$current_player.symbol}" 
+else 
+  return "Choose another cell"
+end
+when 8
+if @cells[0][1].empty?
+  @cells[0][1] = "#{$current_player.symbol}" 
+else 
+  return "Choose another cell"
+end
+when 9
+if @cells[0][2].empty?
+  @cells[0][2] = "#{$current_player.symbol}" 
+else 
+  return "Choose another cell"
+end
+end
+end
+end
+
+class Game
+  attr_accessor :active_board
+  def initialize
+    # @active_board = Board.new
+  end
+  
+  def players_info
+    puts "Player 1 type your name and symbol: "
+    $player_1 = FirstSetup.new(gets.chomp, gets.chomp)
+    puts "Player 2 type your name"
+    $player_2 = FirstSetup.new(gets.chomp, gets.chomp)
+  end
+end 
+
 
