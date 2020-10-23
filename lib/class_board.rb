2e912a1c55561897c9c7a6c3ea459ec5f595fbc0
410AngelaVu <andjela1993@gmail.com>
2020-10-24 00:05:02 +0200

    Fixing the class issues for the code review

---
+++ lib/class_board.rb
@@ -0,0 +1,11 @@
+# This method displays board
+
+def display_board_game(arr)
+  system 'clear'
+  puts "  |  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}  |"
+  puts '  |     |     |     |'
+  puts "  |  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}  |"
+  puts '  |     |     |     |'
+  puts "  |  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}  |"
+  puts '  |     |     |     |'
+end
---
+++ lib/game_class.rb
@@ -0,0 +1,38 @@
+# This is the Game Class
+class Game
+  attr_accessor :game_on
+  attr_reader :cells, :winner, :draw
+
+  def initialize
+    @game_on = true
+    @winner = false
+    @draw = false
+    @cells = %w[1 2 3 4 5 6 7 8 9]
+  end
+
+  def get_move(move, symbol)
+    if cells.any? { |n| move == n } && move.to_i != 0
+      cells[move.to_i - 1] = symbol
+      true
+    else
+      false
+    end
+  end
+
+  def winner_check
+    @win1 = cells[0] == cells[1] && cells[1] == cells[2]
+    @win2 = cells[3] == cells[4] && cells[4] == cells[5]
+    @win3 = cells[6] == cells[7] && cells[7] == cells[8]
+    @win4 = cells[0] == cells[4] && cells[4] == cells[8]
+    @win5 = cells[2] == cells[4] && cells[4] == cells[6]
+    @win6 = cells[0] == cells[3] && cells[3] == cells[6]
+    @win7 = cells[1] == cells[4] && cells[4] == cells[7]
+    @win8 = cells[2] == cells[5] && cells[5] == cells[8]
+
+    @winner = true if @win1 || @win2 || @win3 || @win4 || @win5 || @win6 || @win7 || @win8
+  end
+
+  def draw_check
+    @draw = true if cells.all? { |space| %w[X O].include? space }
+  end
+end
