6c62468bfe5daff1da91643b5774e2ce79f57332
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-28 10:41:42 -0500

    fixed rubocop offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -46,4 +45,4 @@ RSpec.configure do |config|
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
-  end
+end
