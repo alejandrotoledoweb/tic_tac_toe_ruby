9eee6267c2bd5fe0f62abcb28f46835e08c4328f
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-28 09:34:43 -0500

    added first test for game

--- ruby_testing/game_testing.rb
+++ ruby_testing/game_testing.rb
@@ -1,13 +1,12 @@
-require './lib/game_class'
-require './lib/class_board'
-require './lib/set_up'
+# spec/game_spec.rb
+
+require '../lib/game_class'
 
 describe Game do
-  let(:move) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
-  let(:symbol) { 'X' }
-  describe '#game_move' do
-    it 'input on player and change with input' do
-      expect(get_move.add(move, symbol)). to eql(%w[X X X X X X X X X])
+  describe '#get_move' do
+    it 'returns true if there is a valid choosen spot' do
+      game = Game.new
+      expect(game.get_move('1', 'X')).to eql(true)
     end
   end
 end
